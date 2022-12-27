f7071df295238e4bea99d332ed545304546e06d7
achour hassiba <hassibaachour98@gmail.com>
2022-12-27 17:17:49 +0100

    configurer la bare de recherche

--- ExpressFoodProject/ExpressFoodApp/views.py
+++ ExpressFoodProject/ExpressFoodApp/views.py
@@ -6,5 +6,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     meal_object = Meal.objects.all()
+    item_name =  request.GET.get('item-name')
+    if item_name !='' and item_name is not None:
+        meal_object = Meal.objects.filter(title__icontains=item_name)
     return render(request, 'expressFoodTemplates/index.html',{'meal_object': meal_object})
 
