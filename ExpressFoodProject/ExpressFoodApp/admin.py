1108332575cd00fbd02de1ba9bdf310d52e944f1
achour hassiba <hassibaachour98@gmail.com>
2022-12-27 14:49:57 +0100

    creeation des class Meal et Cathegorie + mise en place de la clé etrangère + custom du Django admin

--- ExpressFoodProject/ExpressFoodApp/admin.py
+++ ExpressFoodProject/ExpressFoodApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Meal
 
 # Register your models here.
+admin.site.register(Meal)
+admin.site.register(Category)
+
+#class AdminCategory (admin.ModelAdmin):
+ #   list_display = ('name')
+#admin.site.register(AdminCategory)
