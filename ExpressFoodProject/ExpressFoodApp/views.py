f97c6091f3ee40b2e27355d81f3dd42845f878f7
achour hassiba <hassibaachour98@gmail.com>
2022-12-27 12:54:16 +0100

    integration de la base de donné pgsql + migration

--- ExpressFoodProject/ExpressFoodApp/views.py
+++ ExpressFoodProject/ExpressFoodApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Hello and welcome to my first <u>Django App</u> project!</h1>")
+
--- ExpressFoodProject/ExpressFoodProject/settings.py
+++ ExpressFoodProject/ExpressFoodProject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+import pymongo
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,10 +77,16 @@ WSGI_APPLICATION = 'ExpressFoodProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'ella.db.elephantsql.com',
+        'PORT': '',
+        'NAME': 'vyzegzdq',
+        'USER': 'vyzegzdq',
+        'PASSWORD':'p1YyHx3gYJTRV-jPiDNgnzldACrUO3Cp',
+        'ATOMIC_REQUESTS': True,
     }
 }
+
 
 
 # Password validation
