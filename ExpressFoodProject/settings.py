f5cc7e1ced7b81a38d4ff4f80a65117586d8941b
achour hassiba <hassibaachour98@gmail.com>
2022-12-29 15:11:30 +0100

    config-deploy

--- ExpressFoodProject/settings.py
+++ ExpressFoodProject/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import dj_database_url
-import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- ExpressFoodProject/urls.py
+++ ExpressFoodProject/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.template.defaulttags import url
 from django.urls import path, include
 from django.views.static import serve
+
 
 
 urlpatterns = [
     path('',include('ExpressFoodApp.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
